b016f9876e0c56641b72a48eba4e3295f6c35e7d
pdswanson <57888286+pdswanson@users.noreply.github.com>
2020-01-06 21:47:59 -0600

    Completed homework files

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,39 @@
 # This is for PyBank
+import os
+import csv
+
+budget_csv = os.path.join('budget_data.csv')
+
+with open(budget_csv) as budget:
+    reader = csv.reader(budget)
+    next(reader)
+    revenue = []
+    date = []
+    rev_chg = []
+    for row in reader:
+        revenue.append(int(row[1]))
+        date.append(row[0])
+    for x in range(1,len(revenue)):
+        rev_chg.append(revenue[x] - revenue[x-1])
+        avg_chg = sum(rev_chg) / len(rev_chg)
+        max_chg = max(rev_chg)
+        min_chg = min(rev_chg)
+        max_mon = str(date[rev_chg.index(max(rev_chg))+1])
+        min_mon = str(date[rev_chg.index(min(rev_chg))+1])
+
+print("Financial Analysis")
+print("----------------------------")
+print("Total Months:", len(date))
+print("Total: $", int(sum(revenue)))
+print("Average Change: $", format(avg_chg, '.2f'))
+print("Greatest Increase in Profits:", max_mon, "($", max_chg, ")")
+print("Greatest Decrease in Profits:", min_mon, "($", min_chg, ")")
+
+with open("PyBank.txt", "a") as f:
+    print("Financial Analysis", file=f)
+    print("----------------------------", file=f)
+    print("Total Months:", len(date), file=f)
+    print("Total: $", int(sum(revenue)), file=f)
+    print("Average Change: $", format(avg_chg, '.2f'), file=f)
+    print("Greatest Increase in Profits:", max_mon, "($", max_chg, ")", file=f)
+    print("Greatest Decrease in Profits:", min_mon, "($", min_chg, ")", file=f)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,38 @@
 # This is for PyPoll
+import pandas as pd
+
+election = "election_data.csv"
+
+election_pd = pd.read_csv(election)
+
+votes = len(election_pd.index)
+correy = len(election_pd[election_pd["Candidate"] == "Correy"])
+khan = len(election_pd[election_pd["Candidate"] == "Khan"])
+li = len(election_pd[election_pd["Candidate"] == "Li"])
+otooley = len(election_pd[election_pd["Candidate"] == "O'Tooley"])
+winner = election_pd["Candidate"].value_counts().idxmax()
+
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {str(votes)}")
+print("-------------------------")
+print(f"Khan:", format(khan/votes, '.3%'), "(",str(khan),")")
+print(f"Correy:", format(correy/votes, '.3%'), "(",str(correy),")")
+print(f"Li:", format(li/votes, '.3%'), "(",str(li),")")
+print(f"O'Tooley:", format(otooley/votes, '.3%'), "(",str(otooley),")")
+print("-------------------------")
+print(f"Winner: {str(winner)}")
+print("-------------------------")
+
+with open("PyPoll.txt", "a") as f:
+    print("Election Results", file=f)
+    print("-------------------------", file=f)
+    print(f"Total Votes: {str(votes)}", file=f)
+    print("-------------------------", file=f)
+    print(f"Khan:", format(khan/votes, '.3%'), "(",str(khan),")", file=f)
+    print(f"Correy:", format(correy/votes, '.3%'), "(",str(correy),")", file=f)
+    print(f"Li:", format(li/votes, '.3%'), "(",str(li),")", file=f)
+    print(f"O'Tooley:", format(otooley/votes, '.3%'), "(",str(otooley),")", file=f)
+    print("-------------------------", file=f)
+    print(f"Winner: {str(winner)}", file=f)
+    print("-------------------------", file=f)
